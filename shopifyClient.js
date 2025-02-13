7247a60374a172cc0e7cc2a5fad88a43c350f083
Colin Rabyniuk <colinxr@gmail.com>
2025-02-13 16:47:16 -0500

    handle queries to shopify

--- shopifyClient.js
+++ shopifyClient.js
@@ -6,6 +6,34 @@ class ShopifyClient {
     this.accessToken = accessToken
     this.endpoint = `https://${domain}/admin/api/2024-01/graphql.json`
   }
+
+  async query(queryStr, variables = {}) {
+    try {
+      const response = await fetch(this.endpoint, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "X-Shopify-Access-Token": this.accessToken,
+        },
+        body: JSON.stringify({
+          query: queryStr,
+          variables,
+        }),
+      })
+
+      const data = await response.json()
+
+      if (data.errors) {
+        throw new Error(
+          `GraphQL Errors: ${JSON.stringify(data.errors, null, 2)}`
+        )
+      }
+
+      return data.data
+    } catch (error) {
+      throw new Error(`Failed to query Shopify API: ${error.message}`)
+    }
+  }
 }
 
 export default ShopifyClient
